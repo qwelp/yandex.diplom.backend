7e5717e64b1c2da352ab2404900f577f07f127e3
Vladimir Bun <qwelp@mail.ru>
2020-10-11 06:24:25 +0300

    Update corsOptions 8

--- controllers/users.js
+++ controllers/users.js
@@ -35,7 +35,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true });
+      res.cookie('jwt', token, { maxAge: 3600000, signed: true, httpOnly: true, secure: true });
 
       res.send({
         token: req.cookies.jwt
