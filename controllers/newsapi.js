39b84fc72ac835cee5c4f85bd9ad41c517a2b76c
Vladimir Bun <qwelp@mail.ru>
2020-10-15 13:40:25 +0300

    Правки проверяющего 3

--- controllers/newsapi.js
+++ controllers/newsapi.js
@@ -3,11 +3,11 @@ const newsapi = new NewsAPI('2aed608eb8d346bfbc29e2a1fa38a188');
 
 module.exports.getArticlesNewsapiOrg = (req, res, next) => {
   const { q } = req.params;
-  const url = `https://newsapi.org/v2/everything?q=${q}`;
+
+  console.log(q);
 
   newsapi.v2.everything({
-    q: q,
-    country: 'ru'
+    q: q
   }).then(response => {
     res.send(response.articles);
   });
